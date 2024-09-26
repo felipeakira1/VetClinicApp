e8801f218497cf3b1e955dce277edc3957fef138
felipeakira-exe <feakinoz@gmail.com>
2024-09-26 10:40:45 -0300

    feat: Add AnimalRepository fetch to AppointmentsBloc

--- lib/view/bloc/appointments_bloc.dart
+++ lib/view/bloc/appointments_bloc.dart
@@ -1,6 +1,8 @@
 
 
 import 'package:tempvet/domain/models/Appointment.dart';
+import 'package:tempvet/domain/models/animal.dart';
+import 'package:tempvet/repository/animal_repository.dart';
 import 'package:tempvet/repository/appointment_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -26,12 +28,16 @@ class AppointmentsError extends AppointmentsStates {
 
 class AppointmentsBloc extends Bloc<AppointmentsEvents, AppointmentsStates> {
   final AppointmentRepository _appointmentRepository;
+  final AnimalRepository _animalRepository;
 
-  AppointmentsBloc(this._appointmentRepository) : super(AppointmentsLoading()) {
+  AppointmentsBloc(this._appointmentRepository, this._animalRepository) : super(AppointmentsLoading()) {
     on<FetchAppointments>((FetchAppointments event, Emitter<AppointmentsStates> emit) async {
       emit(AppointmentsLoading());
       try {
         List<Appointment> appointments = await _appointmentRepository.fetchAppointments();
+        for(Appointment appointment in appointments) {
+          appointment.animal = (await _animalRepository.fetchAnimalById(appointment.animalId) as Animal?);
+        }
         emit(AppointmentsLoaded(appointments));
       } catch(e) {
         emit(AppointmentsError(e.toString()));
