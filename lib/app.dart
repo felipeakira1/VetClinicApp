a75be84713d767a774b9d0b3f087e8957c868f09
felipeakira-exe <feakinoz@gmail.com>
2024-09-26 10:41:06 -0300

    refactor: Update app.dart and main.dart files

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tempvet/repository/animal_repository.dart';
+import 'package:tempvet/repository/appointment_repository.dart';
+import 'package:tempvet/repository/in_memory/in_memory_animal_repository.dart';
+import 'package:tempvet/repository/in_memory/in_memory_appointment_repository.dart';
+import 'package:tempvet/view/bloc/appointments_bloc.dart';
+import 'package:tempvet/view/pages/home_page.dart';
+import 'package:tempvet/view/pages/login_page.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    AppointmentRepository appointmentRepository = InMemoryAppointmentRepository();
+    AnimalRepository animalRepository = InMemoryAnimalRepository();
+
+    return BlocProvider(
+      create: (context) => AppointmentsBloc(appointmentRepository, animalRepository),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            colorScheme: ColorScheme.fromSwatch()
+                .copyWith(primary: Colors.black, secondary: Colors.yellow),
+          ),
+          home: LoginPage()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tempvet/view/pages/login_page.dart';
+import 'package:tempvet/app.dart';
 
 void main() {
   runApp(const MainApp());
 }
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSwatch().copyWith(primary: Colors.black, secondary: Colors.yellow),
-        
-      ),
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: LoginPage(emailController: emailController, passwordController: passwordController),
-        ),
-      ),
-    );
-  }
-}
-
