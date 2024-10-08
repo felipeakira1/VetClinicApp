c8b9df87e8fe616f41c25eae3bb564b09848b798
felipeakira-exe <feakinoz@gmail.com>
2024-10-08 13:20:46 -0300

    feat: create Veterinarian model

---
+++ lib/domain/models/veterinarian.dart
@@ -0,0 +1,9 @@
+class Veterinarian {
+  final int id;
+  final String name;
+  final String address;
+  final String cpf;
+  final String crmv;
+  
+  Veterinarian({required this.id, required this.name, required this.address, required this.cpf, required this.crmv});
+}
