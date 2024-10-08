2876cf738d7991e8cb00e04f00611ac94aaf3b12
felipeakira-exe <feakinoz@gmail.com>
2024-10-08 12:06:08 -0300

    feat: add guardian repository to appointments bloc

--- lib/app.dart
+++ lib/app.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tempvet/repository/animal_repository.dart';
 import 'package:tempvet/repository/appointment_repository.dart';
+import 'package:tempvet/repository/guardian_repository.dart';
 import 'package:tempvet/repository/in_memory/in_memory_animal_repository.dart';
 import 'package:tempvet/repository/in_memory/in_memory_appointment_repository.dart';
+import 'package:tempvet/repository/in_memory/in_memory_guardian_repository.dart';
 import 'package:tempvet/view/bloc/appointments_bloc.dart';
-import 'package:tempvet/view/pages/home_page.dart';
 import 'package:tempvet/view/pages/login_page.dart';
 
 class MainApp extends StatelessWidget {
@@ -15,9 +16,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     AppointmentRepository appointmentRepository = InMemoryAppointmentRepository();
     AnimalRepository animalRepository = InMemoryAnimalRepository();
+    GuardianRepository guardianRepository = InMemoryGuardianRepository();
 
     return BlocProvider(
-      create: (context) => AppointmentsBloc(appointmentRepository, animalRepository),
+      create: (context) => AppointmentsBloc(appointmentRepository, animalRepository, guardianRepository),
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
--- lib/view/bloc/appointments_bloc.dart
+++ lib/view/bloc/appointments_bloc.dart
@@ -1,10 +1,12 @@
 
 
-import 'package:tempvet/domain/models/Appointment.dart';
+import 'package:tempvet/domain/models/appointment.dart';
 import 'package:tempvet/domain/models/animal.dart';
+import 'package:tempvet/domain/models/guardian.dart';
 import 'package:tempvet/repository/animal_repository.dart';
 import 'package:tempvet/repository/appointment_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tempvet/repository/guardian_repository.dart';
 
 abstract class AppointmentsEvents {}
 
@@ -29,14 +31,16 @@ class AppointmentsError extends AppointmentsStates {
 class AppointmentsBloc extends Bloc<AppointmentsEvents, AppointmentsStates> {
   final AppointmentRepository _appointmentRepository;
   final AnimalRepository _animalRepository;
+  final GuardianRepository _guardianRepository;
 
-  AppointmentsBloc(this._appointmentRepository, this._animalRepository) : super(AppointmentsLoading()) {
+  AppointmentsBloc(this._appointmentRepository, this._animalRepository, this._guardianRepository) : super(AppointmentsLoading()) {
     on<FetchAppointments>((FetchAppointments event, Emitter<AppointmentsStates> emit) async {
       emit(AppointmentsLoading());
       try {
         List<Appointment> appointments = await _appointmentRepository.fetchAppointments();
         for(Appointment appointment in appointments) {
           appointment.animal = (await _animalRepository.fetchAnimalById(appointment.animalId) as Animal?);
+          appointment.guardian = (await _guardianRepository.retrieveGuardianById(appointment.animal!.guardianId) as Guardian?);
         }
         emit(AppointmentsLoaded(appointments));
       } catch(e) {
