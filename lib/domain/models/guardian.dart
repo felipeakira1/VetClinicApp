413f3c362a829f2da4f57a98f8216a242ce05d93
felipeakira-exe <feakinoz@gmail.com>
2024-10-15 12:48:17 -0300

    refactor: update Guardian model with email field

--- lib/domain/models/guardian.dart
+++ lib/domain/models/guardian.dart
@@ -1,18 +1,20 @@
 import 'package:tempvet/domain/models/animal.dart';
 
 class Guardian {
-  final int id;
+  final int? id;
   final String name;
+  final String email;
   final String address;
   final String cpf;
   List<Animal>? animais;
 
-  Guardian({required this.id, required this.name, required this.address, required this.cpf, this.animais});
+  Guardian({this.id, required this.name, required this.email, required this.address, required this.cpf, this.animais});
 
   factory Guardian.fromJson(Map<String, dynamic> json) {
     return Guardian(
       id: json['id'],
       name: json['name'],
+      email: json['email'],
       address: json['address'],
       cpf: json['cpf'],
     );
@@ -22,6 +24,7 @@ class Guardian {
     return {
       'id': id,
       'name': name,
+      'email': email,
       'address': address,
       'cpf': cpf,
     };
